525ab858c35cfa4c2cd9a05465c1e0e8d2827758
Fayeed <fayeed@live.com>
2020-07-13 22:08:16 +0530

    useClipboard hook completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,58 @@
-import * as React from 'react';
+import { useRef, useCallback, MutableRefObject } from "react";
+import { UseClipboardProps } from "./props";
+import deselectCurrent from "toggle-selection";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+export const useClipboard = ({
+  onCopy,
+  text,
+}: UseClipboardProps): [MutableRefObject<any>, () => void] => {
+  const ref = useRef<any>(null);
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+  const onClick = useCallback(() => {
+    deselectCurrent();
 
-  return counter;
+    const range = document.createRange();
+
+    const selection = document.getSelection()!;
+
+    let span = document.createElement("span")!;
+
+    const element = ref.current as HTMLElement;
+
+    if (element) {
+      if (element.tagName === "INPUT" || element.tagName === "TEXTAREA") {
+        span.textContent = (element as HTMLInputElement).value;
+      } else {
+        span.textContent = element.innerText!;
+      }
+    } else if (text) {
+      span.textContent = text!;
+    } else {
+      throw new Error("Both the ref & text were undefined");
+    }
+
+    span.addEventListener("copy", function (e) {
+      e.stopPropagation();
+
+      onCopy(span.textContent!, successful);
+    });
+
+    document.body.appendChild(span);
+
+    range.selectNodeContents(span);
+
+    selection.addRange(range);
+
+    var successful = document.execCommand("copy");
+
+    if (!successful) {
+      throw new Error("Copy command was unsuccessful");
+    } else {
+      span.remove();
+    }
+  }, [text, ref, onCopy]);
+
+  return [ref, onClick];
 };
+
+export default useClipboard;
---
+++ src/props.ts
@@ -0,0 +1,18 @@
+export interface UseClipboardProps {
+  /**
+   * It's callback function that is called after the `copy` command
+   * is executed.
+   *
+   * @param text: The selected clipboard text.
+   * @param result: Indicates wheater the copy was successful or not
+   */
+  onCopy: (text: string, result: boolean) => void;
+
+  /**
+   * The text that needs to copied to the clipboard.
+   * If the ref is set this field is ignored.
+   *
+   * If both are not set it will throw an error.
+   */
+  text?: string;
+}
