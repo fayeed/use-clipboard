64c6a83d428b9321f2d8a3c9447821310ddd1298
Fayeed <fayeed@live.com>
2020-07-14 13:40:55 +0530

    Example updated

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -5,24 +5,15 @@ import { useClipboard } from "use-clipboard";
 import { useState } from "react";
 
 export default function App() {
-  const [copied, setCopied] = useState(false);
-
-  useEffect(() => {
-    setInterval(() => setCopied(false), 5000);
-  }, []);
-
-  const [inputRef, onInputCopy] = useClipboard({
-    onCopy: () => setCopied(true),
+  const input = useClipboard({
+    disableClipboardAPI: true,
   });
 
-  const [textAreaRef, onTextAreaCopy] = useClipboard({
-    onCopy: () => setCopied(true),
-  });
+  const textArea = useClipboard();
 
-  const [, onTextCopy] = useClipboard({
-    onCopy: (text) => {
-      setCopied(true);
-      alert(`Successfully coppied "${text}"`);
+  const text = useClipboard({
+    onSuccess: (text) => {
+      alert(text);
     },
     text: "Hello world",
   });
@@ -30,34 +21,33 @@ export default function App() {
   return (
     <div id="container">
       <h1>
-        <code>useClipboard</code> {copied ? <span>Copied</span> : "Example"}
+        <code>useClipboard</code>{" "}
+        {textArea.isCoppied ? <span>Copied</span> : "Example"}
       </h1>
 
       <div className="itemContainer">
         <h3>Get the content of input, textarea or any text element:</h3>
-        <pre>
-          {`const [inputRef, onInputCopy] = useClipboard({
-  onCopy: () => console.log("Input copied"),
-});`}
-        </pre>
+        <pre>{`const {ref, copy, cut} = useClipboard()`}</pre>
         <label>TextArea</label>
         <textarea
-          ref={textAreaRef}
-          defaultValue={`textarea editing to see some magic happen!`}
+          ref={textArea.ref}
+          defaultValue={`textarea editing to see some magic happen!\nwhat is this!`}
           rows={4}
         />
         <div className="btnContainer">
-          <button onClick={onTextAreaCopy}>copy textarea</button>
-          <span onClick={onTextAreaCopy}>click span to copy</span>
+          <button onClick={textArea.copy}>copy textarea</button>
+          <button onClick={textArea.cut}>cut textarea</button>
+          <button onClick={textArea.clearClipboard}>clear clipboard</button>
         </div>
         <label>Input</label>
         <input
-          ref={inputRef}
+          ref={input.ref}
           defaultValue={`Start editing to see some magic happen! `}
         />
         <div className="btnContainer">
-          <button onClick={onInputCopy}>copy input</button>
-          <span onClick={onInputCopy}>click span to copy</span>
+          <button onClick={input.copy}>copy input</button>
+          <button onClick={input.clear}>cut input</button>
+          <button onClick={input.clearClipboard}>clear clipboard</button>
         </div>
       </div>
 
@@ -67,16 +57,15 @@ export default function App() {
           hook
         </h3>
         <pre>
-          {`const [, onInputCopy] = useClipboard({
-  onCopy: () => console.log("Input copied"),
-  text: "Hello World!"
-});`}
+          {`const {ref, copy, cut} = useClipboard();
+        
+<button onClick={() => copy("Hello World!")}>copy</button>`}
         </pre>
         <label>Hello World!</label>
 
         <div className="btnContainer">
-          <button onClick={onTextCopy}>copy</button>
-          <span onClick={onTextCopy}>click span to copy</span>
+          <button onClick={() => text.copy("Hello World!")}>copy</button>
+          <button onClick={input.clearClipboard}>clear clipboard</button>
         </div>
       </div>
 
