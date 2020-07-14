5c51b082cd7c744c38dd3799193fc2b3f1643502
Fayeed <fayeed@live.com>
2020-07-14 10:00:41 +0530

    Cut to clipboard functionality added

--- src/index.tsx
+++ src/index.tsx
@@ -5,52 +5,80 @@ import deselectCurrent from "toggle-selection";
 export const useClipboard = ({
   onCopy,
   text,
-}: UseClipboardProps): [MutableRefObject<any>, () => void] => {
+}: UseClipboardProps): [
+  MutableRefObject<any>,
+  (action?: "copy" | "cut") => void
+] => {
   const ref = useRef<any>(null);
 
-  const onClick = useCallback(() => {
-    deselectCurrent();
+  const onClick = useCallback(
+    (action = "copy") => {
+      deselectCurrent();
 
-    const range = document.createRange();
+      const range = document.createRange();
 
-    const selection = document.getSelection()!;
+      const selection = document.getSelection()!;
 
-    let span = document.createElement("span")!;
+      let span = document.createElement("span")!;
 
-    const element = ref.current as HTMLElement;
+      const element = ref.current as HTMLElement;
 
-    if (element) {
-      if (element.tagName === "INPUT" || element.tagName === "TEXTAREA") {
-        span.textContent = (element as HTMLInputElement).value;
+      const isInput =
+        element &&
+        (element.tagName === "INPUT" || element.tagName === "TEXTAREA");
+
+      const input = ref.current as HTMLInputElement;
+
+      if (element) {
+        if (isInput) {
+          span.textContent = input.value;
+
+          if (action === "cut") {
+            input.select();
+          }
+        } else {
+          span.textContent = element.innerText!;
+        }
+      } else if (text) {
+        span.textContent = text!;
       } else {
-        span.textContent = element.innerText!;
+        throw new Error("Both the ref & text were undefined");
       }
-    } else if (text) {
-      span.textContent = text!;
-    } else {
-      throw new Error("Both the ref & text were undefined");
-    }
 
-    span.addEventListener("copy", function (e) {
-      e.stopPropagation();
+      span.addEventListener("copy", function (e) {
+        e.stopPropagation();
 
-      if (onCopy) onCopy(span.textContent!, successful);
-    });
+        if (onCopy) onCopy(span.textContent!, true);
+      });
 
-    document.body.appendChild(span);
+      span.addEventListener("cut", function (e) {
+        e.stopPropagation();
 
-    range.selectNodeContents(span);
+        if (action === "cut" && isInput) input.value = "";
 
-    selection.addRange(range);
+        if (onCopy) onCopy(span.textContent!, true);
+      });
 
-    var successful = document.execCommand("copy");
+      document.body.appendChild(span);
 
-    if (!successful) {
-      throw new Error("Copy command was unsuccessful");
-    } else {
-      span.remove();
-    }
-  }, [text, ref, onCopy]);
+      range.selectNodeContents(span);
+
+      selection.addRange(range);
+
+      var successful = document.execCommand(
+        typeof action === "object" ? "copy" : action
+      );
+
+      if (!successful) {
+        throw new Error("Copy command was unsuccessful");
+      } else {
+        span.remove();
+
+        if (action === "cut" && isInput) input.blur();
+      }
+    },
+    [text, ref, onCopy]
+  );
 
   return [ref, onClick];
 };
