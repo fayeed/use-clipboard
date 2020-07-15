64153c09cbcde7df35a6cebdfa1980ffb513d42a
Fayeed <fayeed@live.com>
2020-07-15 17:27:25 +0530

    Example updated

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -4,72 +4,106 @@ import "./index.css";
 import { useClipboard } from "use-clipboard";
 import { useState } from "react";
 
+const examples = [
+  `// Basic example to copy the content of element to the clipboard.
+import React from "react";
+import useClipboard from "use-clipboard-hook";
+  
+export function App() {
+  const {copy, ref} = useClipboard();
+  
+  return (
+    <div>
+      <textarea ref={ref}/>
+      <button onClick={copy}>copy</button>
+    </div>
+  );
+}`,
+  `// Advanced example
+import React from "react";
+import useClipboard from "use-clipboard-hook";
+
+export function App() {
+  const {copy, cut, clearClipboard, ref} = useClipboard({
+    onSucess: (text) => alert(text),
+    onError: (error) => alert(error),
+    disableClipboardAPI: true, // disables the new clipboard api
+    copiedDuration: 1000 // revert backs the isCopied to false
+  });
+
+  return (
+    <div>
+      <textarea ref={ref}/>
+      <button onClick={copy}>copy</button>
+      <button onClick={cut}>cut</button>
+      <button onClick={clearClipboard}>clear</button>
+    </div>
+  );
+}`,
+  `// overrides the elements text to be coppied.
+import React from "react";
+import useClipboard from "use-clipboard-hook";
+
+export function App() {
+  const {copy, ref} = useClipboard();
+
+  return (
+    <div>
+      <textarea ref={ref}/>
+      <button onClick={() => copy("Hello World!")}>copy</button>
+    </div>
+  );
+}`,
+];
+
 export default function App() {
+  const [inputText, setInput] = useState(examples[0]);
+  const [example, setExample] = useState(0);
   const input = useClipboard({
     disableClipboardAPI: true,
+    onSuccess: () => {
+      alert("Successfully coppied to clipboard.");
+    },
   });
+
+  useEffect(() => setInput(examples[example]), [example]);
 
   const textArea = useClipboard();
-
-  const text = useClipboard({
-    onSuccess: (text) => {
-      alert(text);
-    },
-    text: "Hello world",
-  });
 
   return (
     <div id="container">
       <h1>
         <code>useClipboard</code>{" "}
-        {textArea.isCoppied ? <span>Copied</span> : "Example"}
       </h1>
 
-      <div className="itemContainer">
-        <h3>Get the content of input, textarea or any text element:</h3>
-        <pre>{`const {ref, copy, cut} = useClipboard()`}</pre>
-        <label>TextArea</label>
-        <textarea
-          ref={textArea.ref}
-          defaultValue={`textarea editing to see some magic happen!\nwhat is this!`}
-          rows={4}
-        />
-        <div className="btnContainer">
-          <button onClick={textArea.copy}>copy textarea</button>
-          <button onClick={textArea.cut}>cut textarea</button>
-          <button onClick={textArea.clearClipboard}>clear clipboard</button>
+      <div className="npm" onClick={input.copy}>
+        <code ref={input.ref}>npm i use-clipboard-hook</code>
+        <span>copy</span>
+      </div>
+
+      <div className="codeContainer">
+        <div className="code">
+          <textarea
+            ref={textArea.ref}
+            rows={23}
+            value={inputText}
+            readOnly
+          ></textarea>
         </div>
-        <label>Input</label>
-        <input
-          ref={input.ref}
-          defaultValue={`Start editing to see some magic happen! `}
-        />
-        <div className="btnContainer">
-          <button onClick={input.copy}>copy input</button>
-          <button onClick={input.cut}>cut input</button>
-          <button onClick={input.clearClipboard}>clear clipboard</button>
+        <div className="optionContainer">
+          <button onClick={textArea.copy}>Copy code</button>
+          <button onClick={textArea.cut}>Cut code</button>
+          <button onClick={textArea.clearClipboard}>Clear clipboard</button>
+          <button onClick={() => setExample(example + 1 > 2 ? 0 : example + 1)}>
+            Next example
+          </button>
+          <button onClick={() => setExample(example - 1 < 0 ? 2 : example - 1)}>
+            Previous example
+          </button>
         </div>
       </div>
 
-      <div className="itemContainer">
-        <h3>
-          Using no ref: Hello world!, this will copy the text provided to the
-          hook
-        </h3>
-        <pre>
-          {`const {ref, copy, cut} = useClipboard();
-        
-<button onClick={() => copy("Hello World!")}>copy</button>`}
-        </pre>
-        <label>Hello World!</label>
-
-        <div className="btnContainer">
-          <button onClick={() => text.copy("Hello World!")}>copy</button>
-          <button onClick={input.clearClipboard}>clear clipboard</button>
-        </div>
-      </div>
-
-      <textarea rows={5} />
+      <textarea rows={8} placeholder="Paste here..." />
     </div>
   );
 }
